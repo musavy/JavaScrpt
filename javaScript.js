2a3dfa6a17fdf062172fee17e8437ace78d5a46c
musavy <sayed.musavy@integrify.io>
2019-10-04 00:16:49 +0300

    else if statement

--- javaScript.js
+++ javaScript.js
@@ -1,7 +1,10 @@
-let youLikeMeat = true;
-let myNum = 10;
-if (youLikeMeat) {
-  document.write("you like meat");
+let myAge = 30;
+if (myAge == 30) {
+  document.write("you are over 30!");
+} else if (myAge > 20) {
+  document.write("you are over 20!");
+} else if (myAge > 10) {
+  document.write("you are over  10!");
 } else {
-  document.write("you hate meat");
+  document.write("you are not over 10");
 }
