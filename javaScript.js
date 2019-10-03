8e89efc13a4253dd704504e114a6aeec94606a32
musavy <sayed.musavy@integrify.io>
2019-10-04 00:11:20 +0300

    if statement

--- javaScript.js
+++ javaScript.js
@@ -1,3 +1,7 @@
-alert("Hello world");
-
-alert("hi again");
+let youLikeMeat = true;
+let myNum = 10;
+if (youLikeMeat) {
+  document.write("you like meat");
+} else {
+  document.write("you hate meat");
+}
