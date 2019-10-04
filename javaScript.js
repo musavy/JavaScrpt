85317de9d36a26dbe3aa8ea605aa5b93c247dce3
musavy <sayed.musavy@integrify.io>
2019-10-04 22:53:39 +0300

    logical operators

--- javaScript.js
+++ javaScript.js
@@ -1,10 +1,6 @@
-let myAge = 30;
-if (myAge == 30) {
-  document.write("you are over 30!");
-} else if (myAge > 20) {
-  document.write("you are over 20!");
-} else if (myAge > 10) {
-  document.write("you are over  10!");
+let myAge = 26;
+if (myAge < 18 || myAge > 30 || myAge === 25) {
+  document.write("you aint coming");
 } else {
-  document.write("you are not over 10");
+  document.write("you can come, you cool dude");
 }
