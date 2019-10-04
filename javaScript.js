afdacd9380346263ce84358ac661ea3ea7adce8e
musavy <sayed.musavy@integrify.io>
2019-10-04 23:32:53 +0300

    function

--- javaScript.js
+++ javaScript.js
@@ -1,4 +1,8 @@
-let links = document.getElementsByTagName("a");
-for (i = 0; i < links.length; i++) {
-  links[i].className = "link-" + i;
+function getAverage(a, b) {
+  let average = (a + b) / 2;
+  console.log(average);
+  return average;
 }
+
+let myResult = getAverage(7, 8);
+console.log("the average is " + myResult);
