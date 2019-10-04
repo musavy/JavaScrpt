2752871046c37f29ffc079a7c29462f835c24698
musavy <sayed.musavy@integrify.io>
2019-10-04 23:41:57 +0300

    varibale scope

--- javaScript.js
+++ javaScript.js
@@ -1,8 +1,14 @@
+// variable , we have tow kind of variable 1= global variable is on the top function 2= local variable which define inside of function
+
 function getAverage(a, b) {
-  let average = (a + b) / 2;
-  console.log(average);
+  let average = (a, b) / 2; //local varibale
   return average;
 }
 
-let myResult = getAverage(7, 8);
-console.log("the average is " + myResult);
+let myResult = getAverage(7, 11); //global varibale
+
+function logResult() {
+  console.log("the average" + myResult + "inside the function");
+}
+
+logResult();
