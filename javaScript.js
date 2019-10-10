c744cc5f6b79577a2ec564ac2ffdb5909040bd5f
musavy <sayed.musavy@integrify.io>
2019-10-10 22:30:08 +0300

    not number

--- javaScript.js
+++ javaScript.js
@@ -1,13 +1,10 @@
-let a = 7;
+let a = "apple";
 let b = 5;
 
-console.log(a + b);
+if (isNaN(a)) {
+  console.log("that aint even a number, thickie");
+} else {
+  console.log("meaning of life is " + a * b);
+}
 
-console.log(typeof (a + b));
-
-console.log(Math.round(7.5));
-
-console.log(Math.ceil(7.2));
-console.log(Math.floor(7.2));
-console.log(Math.max(7, 5, 10, 12));
-console.log(Math.PI);
+console.log(a * b);
