2857085bcda1a7a180526825e4a697ff8826c987
musavy <sayed.musavy@integrify.io>
2019-10-13 23:25:18 +0300

    creating object

--- javaScript.js
+++ javaScript.js
@@ -1,10 +1,36 @@
-//obJects
-//Strings,number,arrays,plus many more
+//Creating new object
 
-let myCar = new Car();
+let myArray = new Array();
 
-let myString = new String();
+myArray[0] = 8;
+myArray[1] = "hello";
 
-myString = "Hello";
+//now we using same like array objects
 
-console.log(myString);
+let myCar = new Object();
+myCar.maxSpeed = 50;
+myCar.drive = "masood";
+
+console.log(myCar.drive);
+
+myCar.drive = function() {
+  console.log("now driving");
+};
+
+myCar.drive();
+
+myCar.speed = function() {
+  console.log("your to speed");
+};
+myCar.speed();
+
+let myCar2 = {
+  maxSpeed: 50,
+  driver: "masood",
+  drive: function(speed, time) {
+    console.log(speed, time);
+  }
+};
+
+console.log(myCar2.maxSpeed);
+console.log(myCar2.drive(50, 3));
