241437b64ee511f0763d3a1faee5c0d724b87f72
musavy <sayed.musavy@integrify.io>
2019-10-13 23:47:41 +0300

    functions

--- javaScript.js
+++ javaScript.js
@@ -1,36 +1,19 @@
-//Creating new object
+//constructor functions
 
-let myArray = new Array();
-
-myArray[0] = 8;
-myArray[1] = "hello";
-
-//now we using same like array objects
-
-let myCar = new Object();
-myCar.maxSpeed = 50;
-myCar.drive = "masood";
-
-console.log(myCar.drive);
-
-myCar.drive = function() {
-  console.log("now driving");
+let Car = function(maxSpeed, driver) {
+  this.maxSpeed = maxSpeed;
+  this.driver = driver;
+  this.drive = function(speed, time) {
+    console.log(speed * time);
+  };
+  this.logDriver = function() {
+    console.log("dirver name is " + this.driver);
+  };
 };
 
-myCar.drive();
-
-myCar.speed = function() {
-  console.log("your to speed");
-};
-myCar.speed();
-
-let myCar2 = {
-  maxSpeed: 50,
-  driver: "masood",
-  drive: function(speed, time) {
-    console.log(speed, time);
-  }
-};
-
-console.log(myCar2.maxSpeed);
-console.log(myCar2.drive(50, 3));
+let myCar = new Car(70, "masood");
+let myCar2 = new Car(100, "miran");
+let myCar3 = new Car(80, "morsal");
+let myCar4 = new Car(90, "sahel");
+myCar.drive(30, 5);
+myCar3.logDriver();
